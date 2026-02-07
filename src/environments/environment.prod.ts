379d0000a4d4051f8e6365f84ab588663c4e4d11
Safynazibrahim <safynazibrahim4@gmail.com>
2026-02-07 20:51:11 +0200

    handle environment of prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,17 @@
 export const environment = {
-    production: false,
-    apiUrl: 'https://backend-staging-47c6.up.railway.app/api',
+  production: true,
+  apiUrl: 'https://backend-staging-47c6.up.railway.app/api',
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyDMWzVkMlmgNft4pKNvCpuC_y383M21mRk',
+    authDomain: 'one-ai-5f96b.firebaseapp.com',
+    projectId: 'one-ai-5f96b',
+    storageBucket: 'one-ai-5f96b.firebasestorage.app',
+    messagingSenderId: '61536759010',
+    appId: '1:61536759010:web:f3bc064fcf5ef2930e42bb',
+    measurementId: 'G-FPEQJC3V9T',
+  },
+
+  vapidKey:
+    'BBFeb0YagBNutvnZpiCRxdAndRRl74UKxgpN4iSz-j1ThiArpiFmnSUCP6le5ar1cvvqBN0LLW93JqcgeXlGPE4',
 };
