d27db1943ba8f34de03358af3d529352645fd318
Safynazibrahim <safynazibrahim4@gmail.com>
2026-04-09 22:55:00 +0200

    fix: deploy fileand env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
 export const environment = {
   production: true,
   apiUrl: 'https://backend-staging-47c6.up.railway.app/api',
-  geminiApiKey: "$GEMINI_API_KEY",
+  geminiApiKey: "__GEMINI_API_KEY__",
   firebaseConfig: {
-    apiKey: "$FIREBASE_API_KEY",
+    apiKey: "__FIREBASE_API_KEY__",
     authDomain: 'one-ai-5f96b.firebaseapp.com',
     projectId: 'one-ai-5f96b',
     storageBucket: 'one-ai-5f96b.firebasestorage.app',
     messagingSenderId: '61536759010',
-    appId: "$FIREBASE_APP_ID",
-    measurementId: "$FIREBASE_MEASUREMENT",
+    appId: "__FIREBASE_APP_ID__",
+    measurementId: "__FIREBASE_MEASUREMENT__",
   },
-  vapidKey: "$VAPID_KEY",
+  vapidKey: "__VAPID_KEY__",
 };
